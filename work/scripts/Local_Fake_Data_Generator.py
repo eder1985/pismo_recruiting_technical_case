55236229cc837e93e7d6a6591ae8fac03e1df4a4
Eder Nogueira <eder.nogueira@thoughtworks.com>
2023-07-25 19:39:07 -0300

    update local notebook and local scripts with updates from colab notebook

--- work/scripts/Local_Fake_Data_Generator.py
+++ work/scripts/Local_Fake_Data_Generator.py
@@ -22,14 +22,11 @@ class CustomUUIDProvider(BaseProvider):
     def custom_uuid(self):
         list_uuids = [
             '1a1a1a1a-1a1a-1a1a-1a1a-1a1a1a1a1a1a',
-            '2b2b2b2b-2b2b-2b2b-2b2b-2b2b2b2b2b2b',
-            '3c3c3c3c-3c3c-3c3c-3c3c-3c3c3c3c3c3c',
-            '4d4d4d4d-4d4d-4d4d-4d4d-4d4d4d4d4d4d', 
-            '5e5e5e5e-5e5e-5e5e-5e5e-5e5e5e5e5e5e'
+            '2b2b2b2b-2b2b-2b2b-2b2b-2b2b2b2b2b2b'
             ]
         return random.choice(list_uuids)
 
-def write_fake_data(fake, length, destination_path, unique_uuid = True):  
+def write_fake_data(fake, length, destination_path, unique_uuid = True):
 
     database = []
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -37,11 +34,14 @@ def write_fake_data(fake, length, destination_path, unique_uuid = True):
 
     for x in range(length):
         uuid = fake.uuid4() if unique_uuid else fake.custom_uuid()
+        project_domain_name = fake.project_domain_name()
+        event_type = project_domain_name + "-status-change"
+
         database.append(collections.OrderedDict([
             ('event_id', uuid),
             ('timestamp', datetime.strftime(fake.date_time_between(start_date='-3y', end_date='now'),"%Y-%m-%dT%H:%M:%S")),
-            ('domain', fake.project_domain_name()),
-            ('event_type', "status-change"),
+            ('domain', project_domain_name),
+            ('event_type', event_type),
             ('data', collections.OrderedDict([
                 ('id', fake.random_number(digits=6)),
                 ('old_status', fake.status_type()),
@@ -52,7 +52,7 @@ def write_fake_data(fake, length, destination_path, unique_uuid = True):
 
     with open('%s%s.json' % (destination_path, filename), 'w') as output:
         output.write(dumps(database, indent=4, sort_keys=False, default=str))
-                
+
     print("Done.")
 
 def read_fake_data(json_filepath):
@@ -61,7 +61,6 @@ def read_fake_data(json_filepath):
     return df
 
 def run(unique_uuid = True):
-
     fake = Faker()
     Faker.seed(random.randrange(0, 99999999999999999999, 1))
     fake.add_provider(ProjectDomainProvider)
@@ -69,7 +68,7 @@ def run(unique_uuid = True):
     fake.add_provider(CustomUUIDProvider)
 
     length = 10
-    destination_path = 'C:/Users/Eder/Documents/EDER/projetos/pismo_recruiting_technical_case/work/data/input/'
+    destination_path = 'C:/Users/Eder/Documents/EDER/projetos/pismo_recruiting_technical_case/work/data/raw/events/'
     write_fake_data(fake, length, destination_path,unique_uuid)
 
     json_filepath = destination_path+'*.json'
